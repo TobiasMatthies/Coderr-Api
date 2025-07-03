aebf67549acbaa069bfbe670bfcae4a1236a44d2
TobiasMatthies <tbsmatthies@gmail.com>
2025-07-03 13:58:26 -0300

    added login view

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from users.views import RegistrationView
+from users.views import RegistrationView, LoginView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from users.api.serializers import UserRegistrationSerializer
+from rest_framework.views import APIView
 # Create your views here.
 
 
@@ -11,6 +12,20 @@ class RegistrationView(ObtainAuthToken):
         serializer = UserRegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'username': user.username,
+        })
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
