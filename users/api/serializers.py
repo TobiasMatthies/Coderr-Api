3128983c07eb5264e11cf2818047b834e0a290f8
TobiasMatthies <tbsmatthies@gmail.com>
2025-07-03 14:21:28 -0300

    added type validation for registration

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,7 +6,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'repeated_password']
+        fields = ['username', 'email', 'password', 'repeated_password', 'type']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -14,12 +14,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs['password'] != attrs['repeated_password']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        if attrs['type'] not in dict(User.TYPE_CHOICES):
+            raise serializers.ValidationError({"type": "Invalid user type."})
+
         return attrs
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            type=validated_data['type']
         )
         return user
