6d01d2b28e19d6fb7b9c7e8eed7e2731f3f7c4c5
TobiasMatthies <tbsmatthies@gmail.com>
2025-07-15 10:41:08 -0300

    added filters for min-price and max-delivery-time

--- offers/api/filters.py
+++ offers/api/filters.py
@@ -1,13 +1,41 @@
 import django_filters
+from django.db.models import Count, Q
 from offers.models import Offer
 
 class OfferFilter(django_filters.FilterSet):
     creator_id = django_filters.NumberFilter(field_name='user', lookup_expr='exact')
-    # currently still returning all offers
-    min_price = django_filters.NumberFilter(field_name='details__price', lookup_expr='gte', distinct=True)
-    max_delivery_time = django_filters.NumberFilter(field_name='details__delivery_time_in_days', lookup_expr='lte', distinct=True)
-
+    min_price = django_filters.NumberFilter(method='filter_min_price')
+    max_delivery_time = django_filters.NumberFilter(method='filter_max_delivery_time')
 
     class Meta:
         model = Offer
         fields = ['creator_id', 'min_price', 'max_delivery_time']
+
+
+    def filter_min_price(self, queryset, name, value):
+        """
+        Filter offers that have all 3 details with a price greater or
+        equal to the given value.
+        """
+        return queryset.annotate(
+            matching_details_count=Count(
+                'details',
+                filter=Q(details__price__gte=value)
+            )
+        ).filter(
+            matching_details_count=3
+        )
+
+    def filter_max_delivery_time(self, queryset, name, value):
+        """
+        Filter offers that have all 3 details with a delivery time less than
+        or equal to the given value.
+        """
+        return queryset.annotate(
+            matching_details_count=Count(
+                'details',
+                filter=Q(details__delivery_time_in_days__lte=value)
+            )
+        ).filter(
+            matching_details_count=3
+        )
